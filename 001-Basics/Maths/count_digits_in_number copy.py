63b4f3c6cb7f2ab020ef6f31b0af3fe290496c08
Madhur-Jatiya <madhurmoms@gmail.com>
2026-07-18 14:42:43 +0530

    Count digits in a number

---
+++ 001-Basics/Maths/count_digits_in_number copy.py
@@ -0,0 +1,22 @@
+# Count digits in a number
+
+try:
+    number = int(input("Enter a number: "))
+
+    number = abs(number)
+
+    if number == 0:
+        count = 1
+
+    else:
+        count = 0
+
+    while number > 0:
+        count += 1
+        number = number // 10
+
+    print("Number of Digits = ", count)
+
+except ValueError as e:
+    print("Please enter a valid integer number.")
+    print("Error:", e)
