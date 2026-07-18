323f7d17a7323b8c2d261d9149c3c9166bacde6f
Madhur-Jatiya <madhurmoms@gmail.com>
2026-07-18 15:45:42 +0530

    reverse number

--- 001-Basics/Maths/count_digits_in_number.py
+++ 001-Basics/Maths/count_digits_in_number.py
@@ -1,23 +1,44 @@
 # Count digits in a number
 
-try:
-    number = int(input("Enter a number: "))
+import math
+
+
+def count_digits(number):
 
     number = abs(number)
-    print(number)
 
     if number == 0:
-        count = 1
+        return 1
 
-    else:
-        count = 0
+    count = 0
 
     while number > 0:
         count += 1
         number = number // 10
 
-    print("Number of Digits = ", count)
+    return count
 
-except ValueError as e:
-    print("Please enter a valid integer number.")
-    print("Error:", e)
+
+def count_digits_optimal(number):
+    number = abs(number)
+
+    if number == 0:
+        count = 1
+    else:
+        count = int(math.log10(number) + 1)
+
+    return count
+
+
+def main():
+    try:
+        number = int(input("Enter a number: "))
+        print("Number of Digits =", count_digits(number))
+        print("Number of Digits =", count_digits_optimal(number))
+    except ValueError as e:
+        print("Please enter a valid integer number.")
+        print("Error: ", e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 001-Basics/Maths/reverse_a_number.py
@@ -0,0 +1,35 @@
+# Reverse a Number
+
+
+def reverse_number(number):
+
+    if number == 0:
+        return 0
+    elif number > 0:
+        sign = 1
+    else:
+        sign = -1
+
+    number = abs(number)
+
+    rev_number = 0
+
+    while number > 0:
+        rev_number = (rev_number * 10) + (number % 10)
+        number = number // 10
+
+    print("sign ", sign)
+    return rev_number * sign
+
+
+def main():
+    try:
+        number = int(input("Enter a number: "))
+        print("Reverse Number =", reverse_number(number))
+    except ValueError as e:
+        print("Please enter a valid integer number.")
+        print("Error: ", e)
+
+
+if __name__ == "__main__":
+    main()
